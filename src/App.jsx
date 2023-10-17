3d6dc1e10dad54faaf1d9af9e44fa19a2fb23090
emmanuelniasse <emmanuelniasse@gmail.com>
2023-10-18 01:38:54 +0200

    login / signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+// Pages
 import Expenses from './pages/Expenses/Expenses';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
 
 export default function App() {
     return (
         <Routes>
-            <Route exact path='/' element={<Expenses />} />
+            <Route exact path='/' element={<Login />} />
+            <Route exact path='/inscription' element={<Signup />} />
+            <Route exact path='/depenses' element={<Expenses />} />
         </Routes>
     );
 }
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+export default function Login() {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+    return (
+        <>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit(onSubmit)} className='form'>
+                <input
+                    autoComplete='off'
+                    {...register('username')}
+                    placeholder={'Pseudo'}
+                />
+                <input
+                    autoComplete='off'
+                    {...register('password')}
+                    placeholder={'Mot de passe'}
+                    type='password'
+                />
+                <input
+                    type='submit'
+                    className={'btn btn-add'}
+                    value={'Connexion'}
+                />
+            </form>
+        </>
+    );
+}
